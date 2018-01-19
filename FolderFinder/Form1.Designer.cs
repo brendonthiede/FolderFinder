1358325c82578a48d2de70463e1d27ed032046b3
Brendon Thiede <ThiedeB@courts.mi.gov>
2018-01-19 00:09:10 -0500

    Working good enough

--- FolderFinder/Form1.Designer.cs
+++ FolderFinder/Form1.Designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Nested");
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Root", new System.Windows.Forms.TreeNode[] {
-            treeNode7});
-            this.treeView1 = new System.Windows.Forms.TreeView();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -40,25 +36,9 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // treeView1
-            // 
-            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.treeView1.Location = new System.Drawing.Point(12, 53);
-            this.treeView1.Name = "treeView1";
-            treeNode7.Name = "nested0";
-            treeNode7.Text = "Nested";
-            treeNode8.Name = "rootFolder";
-            treeNode8.Text = "Root";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode8});
-            this.treeView1.Size = new System.Drawing.Size(426, 196);
-            this.treeView1.TabIndex = 1;
-            this.treeView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDown);
             // 
             // button1
             // 
@@ -77,7 +57,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(195, 20);
             this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
@@ -93,14 +72,12 @@
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.infoToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(162, 26);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(68, 26);
             // 
             // infoToolStripMenuItem
             // 
             this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
-            this.infoToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
-            this.infoToolStripMenuItem.Text = "Open in Explorer";
-            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(67, 22);
             // 
             // button2
             // 
@@ -112,16 +89,28 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(422, 173);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(450, 261);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.treeView1);
             this.Name = "Form1";
             this.Text = "Folder Finder";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -132,7 +121,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
@@ -140,6 +128,7 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- FolderFinder/Form1.cs
+++ FolderFinder/Form1.cs
@@ -14,7 +14,7 @@ namespace FolderFinder
 {
     public partial class Form1 : Form
     {
-        private SortedList<string, FolderInfo> folderList = new SortedList<string, FolderInfo>();
+        private List<string> folderList = new List<string>();
 
         public Form1()
         {
@@ -26,7 +26,7 @@ namespace FolderFinder
             updateRootFolder();
         }
 
-        public static void ProcessDirectory(string targetDirectory, TreeNode parentNode)
+        public static void ProcessDirectory(string targetDirectory, List<String> folderList)
         {
             try
             {
@@ -34,46 +34,12 @@ namespace FolderFinder
                 string[] subdirectoryEntries = System.IO.Directory.GetDirectories(targetDirectory);
                 foreach (string subdirectory in subdirectoryEntries)
                 {
-                    string newBase = subdirectory.Substring(targetDirectory.Length);
-                    if (newBase.StartsWith("\\"))
-                    {
-                        newBase = newBase.Substring(1);
-                    }
-                    TreeNode newNode = new TreeNode(newBase);
-                    newNode.Tag = subdirectory;
-                    parentNode.Nodes.Add(newNode);
-                    ProcessDirectory(subdirectory, newNode);
-                }
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        public static void ProcessDirectory(string targetDirectory, SortedList<String, FolderInfo> folderList)
-        {
-            try
-            {
-                // Recurse into subdirectories of this directory.
-                string[] subdirectoryEntries = System.IO.Directory.GetDirectories(targetDirectory);
-                foreach (string subdirectory in subdirectoryEntries)
-                {
-                    folderList.Add(subdirectory, new FolderInfo(subdirectory, targetDirectory));
+                    folderList.Add(subdirectory);
                     ProcessDirectory(subdirectory, folderList);
                 }
             }
-            catch (UnauthorizedAccessException ex)
+            catch (UnauthorizedAccessException)
             {
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
             }
         }
 
@@ -81,58 +47,17 @@ namespace FolderFinder
         {
             string rootFolder = this.folderBrowserDialog1.SelectedPath;
             folderList.Clear();
-            folderList.Add(rootFolder, new FolderInfo(rootFolder, null));
+            folderList.Add(rootFolder);
             ProcessDirectory(rootFolder, folderList);
         }
 
-        private void resetTreeView()
+        private void loadTreeView(string[] masterList)
         {
-            this.treeView1.Nodes.Clear();
-            this.treeView1.Nodes.Add(this.folderBrowserDialog1.SelectedPath);
-            ProcessDirectory(this.folderBrowserDialog1.SelectedPath, this.treeView1.Nodes[0]);
-            this.treeView1.Nodes[0].Expand();
-        }
-
-        private void loadTreeView(SortedList<string, FolderInfo> masterList)
-        {
-            treeView1.Nodes.Clear();
-            masterList.Keys.ToList<string>().ForEach(delegate (String path)
+            listBox1.Items.Clear();
+            for (int i = 0; i < masterList.Length; i++)
             {
-                FolderInfo folderInfo = new FolderInfo(path, null);
-                masterList.TryGetValue(path, out folderInfo);
-                if (folderInfo.parent != null)
-                {
-                    TreeNode newNode = new TreeNode
-                    {
-                        Tag = folderInfo.path,
-                        Text = folderInfo.name
-                    };
-                    folderInfo.parentNode.Nodes.Add(newNode);
-
-                    FolderInfo[] childFolders = folderList.Values.Cast<FolderInfo>().Where(children => children.parent == path).ToArray();
-                    for (int i = 0; i < childFolders.Length; i++)
-                    {
-                        childFolders[i].parentNode = newNode;
-                    }
-
-                }
-                else
-                {
-                    TreeNode rootNode = new TreeNode
-                    {
-                        Tag = path,
-                        Text = path
-                    };
-                    treeView1.Nodes.Add(rootNode);
-                    FolderInfo[] childFolders = folderList.Values.Cast<FolderInfo>().Where(children => children.parent == path).ToArray();
-                    for (int i = 0; i < childFolders.Length; i++)
-                    {
-                        childFolders[i].parentNode = rootNode;
-                    }
-                }
-            });
-
-            this.treeView1.Nodes[0].Expand();
+                listBox1.Items.Add(masterList[i]);
+            }
         }
 
         private void updateRootFolder()
@@ -140,9 +65,8 @@ namespace FolderFinder
             DialogResult result = this.folderBrowserDialog1.ShowDialog();
             if (result == DialogResult.OK)
             {
-                //resetTreeView();
                 resetFolderList();
-                loadTreeView(folderList);
+                loadTreeView(folderList.ToArray());
             }
         }
 
@@ -151,39 +75,16 @@ namespace FolderFinder
             updateRootFolder();
         }
 
-        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        private void listBox1_DoubleClick(object sender, EventArgs e)
         {
-            string path = treeView1.SelectedNode.Text;
-            if (treeView1.SelectedNode.Tag != null)
-            {
-                path = treeView1.SelectedNode.Tag.ToString();
-            }
-            Process.Start(@path);
-        }
-
-        private void treeView1_MouseDown(object sender, MouseEventArgs e)
-        {
-            // Make sure this is the right button.
-            if (e.Button != MouseButtons.Right) return;
-
-            // Select this node.
-            TreeNode node_here = treeView1.GetNodeAt(e.X, e.Y);
-            treeView1.SelectedNode = node_here;
-
-            // See if we got a node.
-            if (node_here == null) return;
-
-            contextMenuStrip1.Show(treeView1, new Point(e.X, e.Y));
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            //treeView1.Nodes[0];
+            Process.Start(@listBox1.SelectedItem.ToString());
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            string filter = textBox1.Text;
+            string[] filtered = folderList.Cast<string>().Where(key => key.ToLowerInvariant().Contains(filter.ToLowerInvariant())).ToArray();
+            loadTreeView(filtered);
         }
     }
 }
